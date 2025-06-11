fd3a5ea47937695684c24de63038f381548d583a
charitha <charitha57807@gmail.com>
2025-06-11 21:59:36 +0530

    linking js to html file

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener("scroll",()=>{
+    document.querySelectorAll("section").forEach(sec=>{
+        const rect=sec.getBoundingClientRect();
+        if(rect.top<window.innerHeight-100){
+            sec.style.opacity=1;
+            sec.style.transform="translateY(0)";
+        }
+    });
+});
+
+//scroll animation
+const sections= document.querySelectorAll("section");
+
+const revealOnScroll=()=>{
+    sections.forEach((sec,index)=>{
+        const rect=sec.getBoundingClientRect();
+        if(rect.top< window.innerHeight-100){
+            sec.style.opacity=1;
+            sec.style.transform="translateY(0)";
+            sec.style.transitionDelay=`${index*0.2}s`;//staggered effect
+        }
+    });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+//initial state
+document.querySelectorAll("section").forEach(sec=>{
+    sec.style.opacity=0;
+    sec.style.transform-"translateY(50px)"
+    sec.style.transition="all 0.6s ease-out";
+});
